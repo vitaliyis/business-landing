f6a363c6ce272bf21be86e0b63c913e7e6289333
vitaliyis <vetal.friend@gmail.com>
2018-09-12 22:00:09 +0300

    Navigation was done

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@
     // Add event listener for open form button
     var openFormButton = document.querySelector('.arrow-down');
     var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
 
     if (openFormButton) {
         openFormButton.addEventListener('click', function (e) {
@@ -19,6 +20,19 @@
                 console.log('Is not valid');
             }
         })
+    }
+
+    if (nav) {
+        nav.addEventListener('click', function (e) {
+            var target = e.target;
+
+            if (target.tagName.toLowerCase() !== 'a') {
+                return;
+            }
+
+            e.preventDefault();
+            ITVDN.navigation.toggleToActiveLink(target);
+        });
     }
 
 }());
